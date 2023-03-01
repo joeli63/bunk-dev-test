7de7fe441d9ad192464c119b94ce80a7dfcc743c
joeli <joelideveloper@gmail.com>
2023-03-01 03:27:30 -0600

    create app module for main routing

--- bunk-expense-tracker/src/app/app-routing.module.ts
+++ bunk-expense-tracker/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ExpenseComponent } from './expense/expense.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: ExpenseComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- bunk-expense-tracker/src/app/app.module.ts
+++ bunk-expense-tracker/src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+
+import { ExpenseModule } from './expense/expense.module';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,9 +13,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
+    ExpenseModule
   ],
-  providers: [],
+  providers: [HttpClientModule],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
